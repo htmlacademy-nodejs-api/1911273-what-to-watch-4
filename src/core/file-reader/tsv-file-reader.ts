6ddfdfc88188d4e511a4878cefcb8a5f73528ea3
rewoop <10rubleqow@gmail.com>
2023-05-06 17:23:31 +0300

    Обновляет обработку импорта фильмов.

--- src/core/file-reader/tsv-file-reader.ts
+++ src/core/file-reader/tsv-file-reader.ts
@@ -1,45 +1,36 @@
-import { readFileSync } from 'node:fs';
+import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { FileReaderInterface } from './file-reader.interface.js';
-import { Film } from '../../types/film.type.js';
-import { Genre } from '../../types/genre.enum.js';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(public filename: string) { }
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
   }
 
-  public toArray(): Film[] {
-    if (!this.rawData) {
-      return [];
+  public async read(): Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([userName, email, avatarPath, title, description, releaseDate, genre, year, rating, preview, video, actors, director, duration, commentsCount, poster, backgroundImage, backgroundColor]) => ({
-        title,
-        description,
-        releaseDate: new Date(releaseDate),
-        genre: Genre[genre as keyof typeof Genre],
-        year: +year,
-        rating: +rating,
-        preview,
-        video,
-        actors: actors.split(','),
-        director,
-        duration: +duration,
-        commentsCount: +commentsCount,
-        poster,
-        background: {
-          image: backgroundImage,
-          color: backgroundColor
-        },
-        user: {name: userName, email, avatarPath},
-      }));
+    this.emit('end', importedRowCount);
   }
 }
